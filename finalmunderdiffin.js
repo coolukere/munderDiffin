c1d3a1651c44127b7e1776c206cccd04a417d5b9
Christine Olukere <coolukere@msn.com>
2018-12-01 01:01:49 -0500

    Add files via upload

---
+++ finalmunderdiffin.js
@@ -0,0 +1,10 @@
+use script ;
+
+let buttons = document.getElementsByClassName('learn-button');
+let paragraphs = document.getElementsByIdName('section-paragraph');
+
+  function show("click", (event)) {
+   button.style.display= 'none' {
+   section-paragraph.style.display = 'block';
+    }
+  }
