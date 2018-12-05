17f1503e2bea8138afcb8fbcf39b8e30a3aaf88e
Christine Olukere <coolukere@msn.com>
2018-12-05 11:50:35 -0500

    Update finalmunderdiffin.js

--- finalmunderdiffin.js
+++ finalmunderdiffin.js
@@ -1,13 +1,15 @@
+"use strict";
 
 button.addEventListener("click", (event) => {
-let paragraph = document.querySelectorAll("column");
-let button = document.getElementsByClassName("learn-button");
-
-if (paragraph.className == "open") {
-          paragraph.className = "";
-           button.innerHTML = "Show More"
-        } else {
-          paragraph.className = "open";
-           button.innerHTML = "Show Less"
-  }
-});
+  let paragraph = document.getElementById("column");
+  let button = document.getElementsByClassName("learn-button");
+   
+  if (paragraph.className == "open") {
+            paragraph.className = "";
+             button.innerHTML = "Show More";
+          } else {
+            paragraph.className = "open";
+             button.innerHTML = "Show Less";
+    }
+  });
+      
